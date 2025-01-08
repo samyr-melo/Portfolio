689ac97242d60d3868b267f672338acb76d94fae
Samyr Melo <153466790+samyr-melo@users.noreply.github.com>
2025-01-08 16:10:00 -0300

    Delete FRONT - DESAFIO 7 directory

--- FRONT - DESAFIO 7/sobre_nos.js
+++
@@ -1,9 +0,0 @@
-const slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
-
- function nextSlide() {
-slides[currentIndex].classList.remove('active');
-currentIndex = (currentIndex + 1) % slides.length; 1 
-slides[currentIndex].classList.add('active'); 2 
- }
- setInterval(nextSlide, 3000)
